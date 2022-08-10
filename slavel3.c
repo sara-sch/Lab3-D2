7dcb895c6b6197eec2a86c01558637adb95567d9
sara-sch <99369223+sara-sch@users.noreply.github.com>
2022-08-09 23:13:29 -0600

    postlab - slave

--- slavel3.c
+++ slavel3.c
@@ -32,45 +32,77 @@
 #include "spi.h"
 #include "oscilador_1.h"
 #include "tmr0.h"
+#include "adc_1.h"
 
 void setup(void);
 
+uint8_t flag = 0;
+uint8_t send_pot1 = 0;
+uint8_t send_pot2 = 0;
+uint8_t adc = 0;
+uint8_t pot1 = 0;
+uint8_t pot2 = 0;
 
 void __interrupt() isr (void){
-    if (PIR1bits.SSPIF){
-
-        PORTD = spiRead();      
-        spiWrite(PORTD << 1);
-    }
-    PIR1bits.SSPIF = 0;             // Limpiamos bandera de interrupción
+   
+        if(PIR1bits.SSPIF){                  // int. SPI
+            if (flag == 1){              // enviamos pot1
+                spiWrite(send_pot1);
+                flag = 0;
+            }
+            else if (flag == 0){         // enviamos pot1
+                spiWrite(send_pot2);
+                flag = 1;
+            }
+            PIR1bits.SSPIF = 0;             // limpiamos bandera de interrupción
+        }
+        else if(PIR1bits.ADIF){
+            if(adc == 0){
+                pot1 = adc_read();          // leemos pot1
+            }
+            else if(adc == 1){
+                pot2 = adc_read();          // leemos pot2
+            }
+        }
+    
     return;
 }
 
 void main(void) {
     setup();
-    
-    while(1){}
+    while(1){
+        if (adc == 0){                // canal 0
+            adc_start(0);
+            adc = 1;
+        }
+        else if (adc == 1){           // canal 1
+            adc_start(1);
+            adc = 0;
+        }
+        send_pot1 = pot1 & 0b11111110;     // indicador de potenciómetros
+        send_pot2 = pot2 | 0b00000001;     
+    }
 }
 
 void setup(void){
-    ANSEL = 0;
+    ANSEL = 0b00000011;
     ANSELH = 0;
     TRISA = 0b00100000;
     PORTA = 0;
     
-    TRISD = 0;
-    PORTD = 0; 
-    
     TRISC = 0b000011000;
     PORTC = 0;
     
-    int_osc_MHz(4);     // Osc a 4 MHz
+    int_osc_MHz(4);                         // Ooscilador a 4 MHz
+    adc_init(1,0,0);
     tmr0_init (256);
     
     INTCONbits.GIE = 1;
     INTCONbits.PEIE = 1;
     PIE1bits.SSPIE = 1;
     PIR1bits.SSPIF = 0; 
+    PIR1bits.ADIF = 0;          
+    PIE1bits.ADIE = 1;          
   
     spiInit(SPI_SLAVE_SS_EN, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_ACTIVE_2_IDLE);
     
