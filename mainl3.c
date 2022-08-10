89fab8a6b9264ce937be5ce16db19c0c0e8fe9c0
sara-sch <99369223+sara-sch@users.noreply.github.com>
2022-08-09 23:13:01 -0600

    postlab - master

--- mainl3.c
+++ mainl3.c
@@ -31,62 +31,101 @@
 #include "spi.h"
 #include "tmr0.h"
 #include "oscilador_1.h"
+#include "adc_1.h"
+#include "lcd.h"
 
 #define _XTAL_FREQ 4000000
+#define FLAG_SPI 0x0F
+
 
 uint8_t cont = 0;
 uint8_t cont2 = 0;
 
 void setup(void);
 
-void __interrupt() isr (void){      // Int. tmr0
-    if(INTCONbits.T0IF){
-        cont++;
-        if (cont == 40){
-            cont = 0; 
-            cont2++; 
-        }
-        PORTB = cont2; 
-    }
-    tmr0_reload();
-    return;
-}
+uint8_t valpot = 0;
+uint8_t pot1 = 0;
+uint8_t pot2 = 0;
+uint8_t indicador = 0;
+uint8_t entero1 = 0;
+uint8_t dec1 = 0;
+uint8_t entero2 = 0;
+uint8_t dec2 = 0;
+unsigned short volt1 = 0;
+unsigned short volt2 = 0;
+char s[];
+
+unsigned short map(uint8_t val, uint8_t in_min, uint8_t in_max, 
+            unsigned short out_min, unsigned short out_max);
+
 
 void main(void) {
+
     setup();
+    Lcd_Clear_8();
+    Lcd_Set_Cursor_8(1,1); // primera fila
+    Lcd_Write_String_8("  POT1    POT2");// escribir en la lcd
     
-    while (1){      
-       
-        PORTCbits.RC2 = 1;
+    
+    while(1){
+        spiWrite(FLAG_SPI); // valor para generar respuesta del slave
+        spiReceiveWait(); // se espera envío
+
+        PORTCbits.RC2 = 1;  // deshabilitamos slave
+        __delay_ms(10);  
+        PORTCbits.RC2 = 0;  // habilitamos slave
         __delay_ms(10);
-        PORTCbits.RC2 = 0;
-        __delay_ms(1000);
-        spiWrite(cont2);
-        PORTD = spiRead();
-          
- 
+
+        spiReceiveWait(); 
+        valpot = spiRead();    // guardamos lo recibido
+        PORTB = valpot;
+        
+        __delay_ms(10);
+        
+        indicador = 0b00000001 & valpot;
+        if (indicador == 1){
+            pot1 = valpot;       //Guardamos pot1 
+            
+            volt1 = map(pot1, 1, 255, 0, 500); // Enteros y decimales de pot1
+            entero1 = volt1/100;
+            dec1 = volt1-entero1*100;
+        }
+        else if (indicador == 0) {
+            pot2 = valpot;      //Guardamos pot2 
+            
+            volt2 = map(pot2, 1, 255, 0, 500);// Enteros y decimales de pot2
+            entero2 = volt2/100;
+            dec2 = volt2-entero2*100;
+        }
+
+        sprintf(s, "  %d.%d    %d.%d ", entero1, dec1, entero2, dec2); // Cadena de valores
+        Lcd_Set_Cursor_8(2,1); // segunda fila
+        Lcd_Write_String_8(s); // escribir en la lcd
+
     }
-              
+    
 }
 
 void setup(void){
     ANSEL = 0;
-    ANSELH = 0;
     
-    TRISD = 0; 
-    PORTD = 0; 
+    TRISD = 0;
+    PORTD = 0;
     
-    TRISB = 0; 
-    PORTB = 0;
-    
-    TRISC = 0b00010000;
+    TRISC = 0b00010001;
     PORTC = 0;
     
-    INTCONbits.T0IE = 1;        // Habiltamos interrupciones
-    INTCONbits.PEIE = 1;
-    INTCONbits.GIE = 1;
+    TRISE = 0;
+    PORTE = 0;
     
-    int_osc_MHz (4);           // Oscilador en 4 MHz 
-    tmr0_init (256);              
-    spiInit(SPI_MASTER_OSC_DIV4,SPI_DATA_SAMPLE_MIDDLE,SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);
+    int_osc_MHz(4);
+    spiInit(SPI_MASTER_OSC_DIV4, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE); 
+    Lcd_Init_8();
+}
+
+
+
+unsigned short map(uint8_t x, uint8_t x0, uint8_t x1,
+            unsigned short y0, unsigned short y1){
+    return (unsigned short)(y0+((float)(y1-y0)/(x1-x0))*(x-x0));
 }
